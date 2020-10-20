44130ddcc77f39164e2c08906cee2cdf112e26bd
ichikawa k <ichikawa.k.al@m.titech.ac.jp>
2020-10-20 16:11:16 +0900

    keyEvent 5 and fix collision

--- src/tetris/scala/tetris.scala
+++ src/tetris/scala/tetris.scala
@@ -74,21 +74,29 @@ case class TetrisWorld(piece: ((Int, Int), S.Shape), pile: S.Shape) extends Worl
   // 2, 5. keyEvent
   // 目的： ブロックの入力受け付け
   def keyEvent(key: String): World = {
+    // var ((x, y), sh) = piece
+    // if(key == "RIGHT") x += 1
+    // if(key == "LEFT") x -= 1
+    // if(key == "UP") sh = S.rotate(sh)
+    // TetrisWorld(((x, y), sh), pile)
+
     var ((x, y), sh) = piece
     if(key == "RIGHT") x += 1
     if(key == "LEFT") x -= 1
     if(key == "UP") sh = S.rotate(sh)
-    TetrisWorld(((x, y), sh), pile)
+    val now = TetrisWorld(piece, pile)
+    val world = TetrisWorld(((x, y), sh), pile)
+    if (collision(world)) TetrisWorld(piece, pile)
+    else world
   }
 
   // 3. collision
   // 目的：はみ出してたりしたらtrue
   def collision(world: TetrisWorld): Boolean = {
-    val ((x, y), sh) = piece
+    val ((x, y), sh) = world.piece
     val (y_sh, x_sh) = S.size(sh)
     val (hei, wid) = S.size(pile)
-    x < 0 || x > wid - x_sh || y > hei - y_sh - 1 || S.overlap(sh, pile)
-    // なぜかy座標は1ずれてる…
+    x < 0 || x > wid - x_sh || y > hei - y_sh || S.overlap(sh, pile)
   }
 
   // 6. eraseRows
